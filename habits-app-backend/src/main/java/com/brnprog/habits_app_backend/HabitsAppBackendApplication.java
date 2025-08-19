92396ee5d9124366ec2019729f83c265712d085e
Breno <brenobarreto5@hotmail.com>
2025-08-19 10:40:30 -0300

    inital commit

---
+++ habits-app-backend/src/main/java/com/brnprog/habits_app_backend/HabitsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brnprog.habits_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitsAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitsAppBackendApplication.class, args);
+	}
+
+}
---
+++ habits-app-backend/src/test/java/com/brnprog/habits_app_backend/HabitsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brnprog.habits_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitsAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
